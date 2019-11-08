1cf8a465d88d1c6341a258f914189bca03a7af03
Basak Altintas <basak1altintas@gmail.com>
2019-11-07 20:29:42 -0600

    TestCases

--- src/test/java/Tests/TestCase4.java
+++ src/test/java/Tests/TestCase4.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import utils.BrowserFactory;
+import utils.VerifyStringEquality;
 
 public class TestCase4 {
     /*Step 1. Go to https://practicecybertekschool.herokuapp.com
@@ -18,5 +19,9 @@ Step 4. Verify that warning message is displayed: “ﬁrst name can only consis
         driver.get("https://practice-cybertekschool.herokuapp.com/");
         driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[40]/a")).click();
         driver.findElement(By.xpath("//*[@id=\"registrationForm\"]/div[1]/label")).sendKeys("123");
+        String expectedErrorMessage = "first name can only consist of alphabetical letters";
+        String actualErrorMessage = driver.findElement(By.xpath("//*[@id=\"registrationForm\"]/div[1]/div/small[3]")).getText();
+        VerifyStringEquality.verifyStrings(expectedErrorMessage, actualErrorMessage);
+        driver.close();
     }
 }
